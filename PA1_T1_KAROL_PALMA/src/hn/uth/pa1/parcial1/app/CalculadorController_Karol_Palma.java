888d801cd37cfa4b3972a8e1bac0db96f409c714
Karol <karolstephpalma@gmail.com>
2020-02-05 22:51:07 -0600

    Guardado Inicial del Proyecto
    Contiene el formulario de inicio completo y el formulario de calculador incompleto.

---
+++ PA1_T1_KAROL_PALMA/src/hn/uth/pa1/parcial1/app/CalculadorController_Karol_Palma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.uth.pa1.parcial1.app;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author KP
+ */
+public class CalculadorController_Karol_Palma {
+    
+    public void cambiarColores(){
+        JLabel lblFondo1 = new JLabel();
+        lblFondo1.setOpaque(true);
+        lblFondo1.setBackground(Color.ORANGE);
+    }
+}
